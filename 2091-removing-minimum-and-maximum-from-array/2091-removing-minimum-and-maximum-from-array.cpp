611f90b16afb6549a119bd39268508914af079e9
bhavyakumar16 <kumarbhavya1605@gmail.com>
2026-08-30 23:11:24 +0530

    Time: 12 ms (6.69%), Space: 92 MB (86.87%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -16,16 +16,16 @@ public:
    int left=min(maxind,minind);
    int right=max(maxind,minind);
 
-   vector<int>ans;
+   
    int front=right+1;
-   ans.push_back(front);
+  
    int back=n-left;
-   ans.push_back(back);
+   
    int both=left+1+n-right;
-   ans.push_back(both);
+  
 
-   int fin=*min_element(ans.begin(),ans.end());
-   return fin;
+   
+   return min(front,min(back,both));
 
         
     }
