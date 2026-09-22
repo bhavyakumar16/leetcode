052e1a154cf2a662d14c3b9eee63779e3fee7c25
bhavyakumar16 <kumarbhavya1605@gmail.com>
2026-09-22 14:32:36 +0530

    Time: 46 ms (46.09%), Space: 88.9 MB (7.66%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        map<int,int>freq;
+        int left = 0 ,right = 0;
+        int ans = 0;
+
+        while(right<n){
+            freq[fruits[right]]++;
+
+            while(freq.size()>2){
+                freq[fruits[left]]--;
+                if(freq[fruits[left]]==0){
+                    freq.erase(fruits[left]);
+                }
+                left++;
+               
+            }
+             ans=max(ans,right-left+1);
+            right++;
+        }
+
+        return ans;
+        
+    }
+};
